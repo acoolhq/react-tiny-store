5c789cf968bc805da280b72e3223a326771d08ea
Anirudh Chintha <anirudhchintha95@gmail.com>
2025-10-11 11:49:02 -0400

    feat: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export { createContextSync } from "./lib/createContextSync";
+export {
+  createContextSync,
+  createStoreSlice,
+  makeStore,
+  useStoreActions,
+  useStoreSelector,
+  bindStoreActions,
+} from "./lib/createContextSync";
+export type { Store, StoreAccess } from "./lib/createContextSync";
